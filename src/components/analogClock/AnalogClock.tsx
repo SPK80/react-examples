05ff5f74d2dd2ebed204c6d50b8aa5968a5a73ff
SPK80 <psukonkin@gmail.com>
2022-08-22 20:17:17 +0300

    feat: implement analog clock with inline styles

--- src/components/analogClock/AnalogClock.tsx
+++ src/components/analogClock/AnalogClock.tsx
@@ -1,17 +1,42 @@
 import React, {useEffect, useState} from "react";
-import s from './analogClock.module.css'
 
 export const AnalogClock: React.FC = () => {
     const [dateTime, setDateTime] = useState<Date>(new Date())
     
     useEffect(() => {
-        const timerId= setInterval(() => setDateTime(new Date()), 1000)
-        return ()=> {
-            clearInterval(timerId)
-        }
+        const timerId = setInterval(() => setDateTime(new Date()), 1000)
+        return () => clearInterval(timerId)
     }, [])
     
-    return <div className={s.analogClock}>
-        {dateTime.toLocaleString()}
-    </div>
+    const seconds = dateTime.getSeconds()
+    const minutes = dateTime.getMinutes()
+    const hours = dateTime.getHours()
+    console.log(hours + ':' + minutes + ':' + seconds)
+    
+    return (
+        <div style={{
+            position: 'relative',
+            width: 200,
+            height: 200,
+            border: 'solid 1px aqua',
+            borderRadius: '50%',
+        }}>
+            <div style={{
+                position: 'absolute',
+                backgroundColor: 'lightgreen',
+                width: 5,
+                height: 100,
+                transformOrigin: 'bottom',
+                transform: `translateX(100px) rotate(${seconds * 6}deg)`,
+            }}></div>
+            <div style={{
+                position: 'absolute',
+                backgroundColor: 'red',
+                width: 5,
+                height: 70,
+                transformOrigin: 'bottom',
+                transform: `translate(100px, 30px) rotate(${hours * 30}deg)`,
+            }}></div>
+        </div>
+    )
 }
